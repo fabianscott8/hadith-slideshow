5d4be82f45f53a87c5d3d2a51c814651877eef68
fabianscott8 <fabianscott8@gmail.com>
2025-03-13 21:39:50 -0400

    Islamic Quotes Update

---
+++ quotes.js
@@ -0,0 +1,90 @@
+// hadithsData.js
+
+// A plain JS array of objects
+const hadiths = [
+  {
+    number: "Book 2, Hadith 8",
+    text: `The Prophet (ﷺ) said <p>"None of you will have faith till he loves me 
+           more than his father, his children and all mankind."`,
+    reference: "[Sahih al-Bukhari 15]"
+  },
+  {
+    number: "Book 2, Hadith 11",
+    text: `
+Narrated 'Ubada bin As-Samit (who took part in the battle of Badr...):
+"Swear allegiance to me for:<p>
+<ol class="left-list">
+  <li>Not to join anything in worship along with Allah.</li>
+  <li>Not to steal.</li>
+  <li>Not to commit illegal sexual intercourse.</li>
+  <li>Not to kill your children.</li>
+  <li>Not to accuse an innocent person...</li>
+  <li>Not to be disobedient (when ordered) to do good deed."</li>
+</ol><p>
+The Prophet (ﷺ) added: 
+"Whoever among you fulfills his pledge...
+'Ubada bin As-Samit added: 'So we swore allegiance for these.'"
+    `,
+    reference: "[Sahih al-Bukhari 18]"
+  },
+  {
+    number: "58:11",
+    text: '"Allah will raise those who have believed among you and those who were given knowledge, by degrees.”',
+    reference: "[Surah Al-Mujadila]"
+  },
+  {
+    number: "Book 2, Hadith 30",
+    text: 'Allah\'s Messenger (ﷺ) said: <p> "Whoever establishes prayers during the nights of Ramadan faithfully out of sincere faith and hoping to attain Allah\'s rewards (not for showing off), all his past sins will be forgiven."',
+    reference: "[Sahih al-Bukhari 37]"
+  },
+  {
+    number: "16:125",
+    text: '“Invite to the way of your Lord with wisdom and good instruction, and argue with them in a way that is best...”',
+    reference: "[Surah An-Nahl]"
+  },
+  {
+    number: "1631",
+    text: 'The Prophet (peace be upon him) said:<p>“When a person dies, his deeds come to an end except for three: ongoing charity, beneficial knowledge, or a righteous child who prays for him.”',
+    reference: "[Sahih Muslim]"
+  },
+  {
+    number: "66:6",
+    text: '"Save yourselves and your families from the Fire"',
+    reference: "[Surah At-Tahrim]"
+  },
+  {
+    number: "3461",
+    text: 'The Prophet (peace be upon him) said:<p>“Convey from me, even if it is one verse.”',
+    reference: "[Sahih al-Bukhari]"
+  },
+  {
+    number: "15:26-27",
+    text: '“And We certainly created man out of clay from an altered black mud. And the jinn We created before from scorching fire.”',
+    reference: "[Surah Al-Hijr]"
+  },
+  {
+    number: "Sahih al-Bukhari 6064",
+    text: 'The Prophet Muhammad (peace be upon him) said:<p>“Do not spy on one another, and do not eavesdrop on one another...”',
+    reference: "[Sahih Muslim 2563]"
+  },
+  {
+    number: "4888",
+    text: '“If you search for people\'s faults, you will corrupt them or at least bring them close to corruption.”',
+    reference: "[Sunan Abu Dawood]"
+  },
+  {
+    number: "4889",
+    text: '“A leader who spies on his people will corrupt them.”',
+    reference: "[Sunan Abu Dawood]"
+  },
+  {
+    number: "5:3",
+    text: '“…This day I have perfected for you your religion and completed My favor upon you and have approved for you Islam as your religion...”',
+    reference: "[Surah Al-Ma'idah]"
+  },
+  {
+    number: "47:24",
+    text: '“…This day I have perfected for you your religion and completed My favor upon you and have approved for you Islam as your religion...”',
+    reference: "[Surah Muhammad]"
+  },
+];
